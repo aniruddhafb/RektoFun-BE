52685de047fcf664d2d1d0e86eb34d39f238d9eb
shravan <shravanandoria21437@gmail.com>
2026-06-11 15:24:01 +0530

    removed unnecessary code

--- migrations/006_add_parent_name_to_markets.sql
+++
@@ -1,5 +0,0 @@
--- Migration: Add parent_name to markets table
--- Created: 2026-05-05
-
-ALTER TABLE public.markets
-ADD COLUMN IF NOT EXISTS parent_name text NULL;
--- routes/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Routes package."""
-
-from routes.clans import router as clans_router
-
-__all__ = ["clans_router"]
